180f191ff79bb02d1b182f82b01b8a063c2711bf
v40yadav <v40yadav@gmail.com>
2018-01-07 19:16:57 +0530

    some minar Changes

---
+++ app/src/main/java/com/example/abdhesh/testone/Hello1.java
@@ -0,0 +1,8 @@
+package com.example.abdhesh.testone;
+
+/**
+ * Created by Abdhesh on 1/7/2018.
+ */
+
+public class Hello1 {
+}
--- app/src/main/java/com/example/abdhesh/testone/MainActivity.java
+++ app/src/main/java/com/example/abdhesh/testone/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.abdhesh.testone;
 
+import android.support.constraint.solver.LinearSystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ProgressBar linearLayout= (ProgressBar) findViewById(R.id.pb);
+
         // Demo chmmggit dsjj
     }
 }
